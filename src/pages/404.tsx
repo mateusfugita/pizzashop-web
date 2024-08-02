a83771872d4c1937bec1e19352ee8bc7e49bfd60
Mateus Fugita <mateushiwao@gmail.com>
2024-08-02 19:37:07 -0300

    feat: create error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,15 @@
-import { Link } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 
 export function NotFound() {
+  const error = useRouteError() as Error
+
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
-      <h1 className="text-4xl font-bold">Página não encontrada</h1>
+      <h1 className="text-4xl font-bold">Whoops, algo aconteceu...</h1>
+      <p className="text-accent-foreground">
+        Um erro aconteceu na aplicação, abaixo você encontra mais detalhes:
+      </p>
+      <pre>{error?.message || JSON.stringify(error)}</pre>
       <p className="text-accent-foreground">
         Voltar para o{' '}
         <Link to="/" className="dark: text-sky-400 text-sky-600">
