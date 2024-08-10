6ca45d95673fef92596c857f1cf405d821e7c7f6
Mateus Fugita <mateushiwao@gmail.com>
2024-08-09 21:06:31 -0300

    feat: create e2e tests for sign in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -35,14 +35,20 @@ export function SignIn() {
   })
 
   async function handleSignIn(data: SignInForm) {
-    await authenticate({ email: data.email })
+    try {
+      await authenticate({ email: data.email })
 
-    toast.success('Enviamos um link de autenticação para seu e-mail.', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => handleSignIn(data),
-      },
-    })
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
